9b8cf53384544a7c462f39848b4865aae613d667
Issa71 <109733928+Issa71@users.noreply.github.com>
2022-07-27 17:15:10 +0100

    Add files via upload

---
+++ TestFiles/TestFactorial.java
@@ -0,0 +1,13 @@
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestFactorial {
+
+	@Test
+	public void FactorialTest() {
+	assertEquals(5, Factorial.Factorial(120));
+	}
+}
