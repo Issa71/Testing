9d8f24f6cd9d08536c6e9c6868943569542198de
Issa71 <109733928+Issa71@users.noreply.github.com>
2022-07-27 17:14:16 +0100

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,24 +1,22 @@
-package factorial;
+
 
 public class Factorial{
+	
 public static void main(String[] args) {
 
-    float[] input = {120, 24, 6, 18};
-    int divider = 2; 
+	
+System.out.println(Factorial(120));
 
-    for(int i = 0; i < input.length; i++) {
-        System.out.print(input[i]);
-        divide(input[i], divider);
-    }
 }
 
-public static void divide(float input, int divider) {
-    if((input / divider) % 1 == 0) {
-        divide(input / divider, ++divider);
-    } else if(input == 1) {
-        System.out.print(" = " + --divider + "!\n");
-    } else {
-        System.out.print(" = NONE\n");
-    }
+public static int Factorial(int value) {
+	
+	for (int i=2; value!= i  ; i++) {
+		
+		value = value/i;
+		
+	}
+	return value;
 }
+
 }
